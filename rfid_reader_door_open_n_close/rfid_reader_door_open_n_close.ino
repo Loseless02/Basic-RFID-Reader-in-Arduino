f091a91183a1d2e57b3849921a5a873a51596533
Atherion <102375123+Loseless02@users.noreply.github.com>
2024-03-14 12:03:44 +0100

    Update rfid_reader_door_open_n_close.ino

--- rfid_reader_door_open_n_close/rfid_reader_door_open_n_close.ino
+++ rfid_reader_door_open_n_close/rfid_reader_door_open_n_close.ino
@@ -1,34 +1,34 @@
-#include <Servo.h>                //Servo kütüphanemizi ekliyoruz.
+#include <Servo.h>                //Adding our servo library
 
-int pirPin = 8;                   //Sensörü takacağımız pin
-int servoPin = 9;                 //Servoyu takacağımız pin
-int hareket;                      //Sensörden aldığımız veri
-int yesilisik = 2;
-int kirmiziisik = 3;
-Servo motor;                      //Servo motor değişkeni
+int pirPin = 8;                   //The pin to attach the sensor to
+int servoPin = 9;                 //The pin to connect the servo
+int movement;                     //Data from the sensor
+int greenLight = 2;
+int redLight = 3;
+Servo motor;                      //Servomotor variable
 
 
 void setup() {
-  motor.attach(servoPin);         //Servomuzu 9. pin ile ilişkilendiriyoruz.
-  pinMode(pirPin, INPUT);         //Sensör pinimizi giriş olarak ayarlyoruz.
+  motor.attach(servoPin);         //Connect the servo to pin 9
+  pinMode(pirPin, INPUT);         //Set the sensor pin as input
 
 }
 
 void loop() {
-  hareket = digitalRead(pirPin);  //Sensörden okuma yapıyoruz.
+  movement = digitalRead(pirPin);  //Reading from the sensor
   
-  if(hareket == LOW){                           //Hareketlilik yok ise içerideki komutlar uygulanır.
-    pinMode(kirmiziisik, HIGH); 
+  if(movement == LOW){                           //If there is no movement, the commands inside are applied
+    pinMode(redLight, HIGH); 
   }
 
-  if(hareket != HIGH){            //Hareketlilik var ise içerideki komutlar uygulanır.
-    pinMode(yesilisik, HIGH);
-    if(hareket != HIGH){                           //Hareketlilik yok ise içerideki komutlar uygulanır.
-      pinMode(kirmiziisik, HIGH); 
+  if(movement != HIGH){            //If there is movement, the commands inside are applied
+    pinMode(greenLight, HIGH);
+    if(movement != HIGH){                           //If there is no movement, the commands inside are applied
+      pinMode(redLight, HIGH); 
   }
   }
-  if(hareket != HIGH){                           //Hareketlilik yok ise içerideki komutlar uygulanır.
-    pinMode(kirmiziisik, HIGH); 
+  if(movement != HIGH){                           //Hareketlilik yok ise içerideki komutlar uygulanır.
+    pinMode(redLight, HIGH); 
   }
 }
 
